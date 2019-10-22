5e677ba047d0711a34664ba37432e07be81920ab
Marvin Ma <yma010@ucr.edu>
2019-10-22 10:35:42 -0700

    Created index route for pets

--- routes/api/pets.js
+++ routes/api/pets.js
@@ -1,6 +1,21 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
+const Pet = require("../../model/Pet");
 
-router.get("/test", (req, res) => res.json({ msg: "This is the pets route" }));
+router.get("/test", (req, res) => res.json({
+  msg: "This is the pets route"
+}));
+
+router.get("/index", (req, res) => {
+  Pet.find({})
+    .then(pets => {
+      if (!pets) {
+        errors.pets = "No Pets Found!"
+        return res.status(404).json(errors)
+      } else {
+        res.json(pets);
+      }
+    })
+})
 
 module.exports = router;
