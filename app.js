535cdb4d32ea07bd0857acc451e90f572f572205
Marvin Ma <yma010@ucr.edu>
2019-10-23 16:35:21 -0700

    Deploy to Heroku

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 const router = express.Router();
 const app = express();
 const mongoose = require("mongoose");
@@ -8,6 +9,7 @@ const users = require("./routes/api/users");
 const pets = require("./routes/api/pets");
 const requests = require("./routes/api/requests");
 const passport = require("passport");
+
 
 mongoose
   .connect(db, { useNewUrlParser: true})
@@ -28,5 +30,11 @@ app.use("/api/requests", requests);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 module.export = app;
--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Modal from './modal/modal'
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch, Route } from 'react-router-dom';
-import NavBarContainer from './nav/navbar_container';
 
+import NavBarContainer from './nav/navbar_container';
 import HomePage from './home/home_page';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
