a587030383920f7bdc92c1b1a70b5151049c89b7
Destiny Rose <destinyrose2358@gmail.com>
2019-10-24 17:24:42 -0500

    Adds the sent and received requests reducers

---
+++ frontend/src/reducers/received_requests_reducer.js
@@ -0,0 +1,25 @@
+import { merge } from "lodash";
+import { APPROVE_RECEIVED_REQUEST, DENY_RECEIVED_REQUEST, RECEIVE_RECEIVED_REQUESTS } from "../actions/requests_actions";
+
+
+export default (state = {}, action) => {
+  Object.freeze(state);
+  let newState;
+  switch (action.type) {
+    case RECEIVE_RECEIVED_REQUESTS:
+      newState = merge({}, state, action.receivedRequests);
+      return newState;
+    case APPROVE_RECEIVED_REQUEST:
+      newState = merge({}, state);
+      delete newState[object.keys(action.approved)[0]];
+      Object.keys(action.denied).forEach(deniedId => {
+        delete newState[deniedId];
+      });
+      return newState;
+    case DENY_RECEIVED_REQUEST:
+      newState = merge({}, state);
+      delete newState[action.deniedRequest]
+    default:
+      return state;
+  }
+};
