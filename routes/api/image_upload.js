45dffc5db59252d8be6ca313529de0a605cf4c81
Marvin Ma <yma010@ucr.edu>
2019-10-24 10:24:21 -0700

    Create image upload functionality

---
+++ routes/api/image_upload.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const upload = require('../../services/image_upload')
+
+router.post('/image-upload', function(req, res){
+
+})
---
+++ services/image_upload.js
@@ -0,0 +1,32 @@
+const aws = require('aws-sdk');
+const express = require('express');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const keys = require('../config/keys')
+
+aws.config.update({
+  secretAccessKey: keys.AWSSecretKey,
+  accessKeyId:keys.AWSAccessKey,
+  region: keys.AWSRegion
+})
+
+const s3 = new aws.S3()
+
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'petsy-cdm-seeds',
+    metadata: function (req, file, cb) {
+      cb(null, {
+        fieldName: file.fieldname
+      });
+    },
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString())
+    }
+  })
+})
+
+
+module.exports = upload;
