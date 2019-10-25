ea25175ea2bfcfd8608e7aa09468a94a3b5bb63c
Destiny Rose <destinyrose2358@gmail.com>
2019-10-25 10:07:29 -0500

    gets shopping kennel to show requested pets

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -7,6 +7,7 @@ import PetsIndexContainer from './pets/pets_index_container';
 import CreatePetContainer from './pets/create_pet_container'
 import PetShowContainer from './pets/pets_show_container';
 import EditPetContainer from './pets/edit_pet_container';
+import ShoppingKennelContainer from './shopping-kennel/shopping_kennel_container';
 
 const App = () => (
   <div>
@@ -19,7 +20,7 @@ const App = () => (
       <Route path="/pets/register" component={CreatePetContainer} />
       <Route path="/pets/edit/:editPetId" component={EditPetContainer} />
       <Route path="/pets/:petId" component={PetShowContainer} />
-      
+      <ProtectedRoute path="/cart" component={ ShoppingKennelContainer } />
     </Switch>
   </div>
 );
