1db1b2100071bba8c28ec1157d2516e3919fd86b
Marvin Ma <yma010@ucr.edu>
2019-10-27 15:24:26 -0700

    Updated User Model

--- model/User.js
+++ model/User.js
@@ -1,6 +1,22 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const defaultURL = [
+  "https: //petsy-cdm-seeds.s3-us-west-1.amazonaws.com/png/bull.png", 
+  "https: //petsy-cdm-seeds.s3-us-west-1.amazonaws.com/png/chick.png" ,
+  "https: //petsy-cdm-seeds.s3-us-west-1.amazonaws.com/png/crab.png", 
+  "https: //petsy-cdm-seeds.s3-us-west-1.amazonaws.com/png/fox.png",
+  "https: //petsy-cdm-seeds.s3-us-west-1.amazonaws.com/png/hedgehog.png",
+  "https: //petsy-cdm-seeds.s3-us-west-1.amazonaws.com/png/hippopotamus.png",
+  "https: //petsy-cdm-seeds.s3-us-west-1.amazonaws.com/png/koala.png",
+  "https: //petsy-cdm-seeds.s3-us-west-1.amazonaws.com/png/lemur.png",
+  "https: //petsy-cdm-seeds.s3-us-west-1.amazonaws.com/png/pig.png",
+  "https: //petsy-cdm-seeds.s3-us-west-1.amazonaws.com/png/tiger.png",
+  "https: //petsy-cdm-seeds.s3-us-west-1.amazonaws.com/png/whale.png",
+  "https: //petsy-cdm-seeds.s3-us-west-1.amazonaws.com/png/zebra.png"
+];
+
+
 const UserSchema = new Schema({
   username: {
     type: String,
@@ -20,6 +36,7 @@ const UserSchema = new Schema({
   }],
   image:{
     type: String,
+    default: defaultURL[Math.floor(Math.random() * 12)]
   },
   date: {
     type: Date,
